cf9d468738189c0afee66e0d932f17bf78f1b09e
shaltaev <shaltaev.gleb@gmail.com>
2019-08-05 10:56:18 +0400

    add testing

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    roots: ['<rootDir>/src'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest'
+    },
+    transformIgnorePatterns: ['<rootDir>/node_modules/(?!(jest-atomic)/)']
+}
